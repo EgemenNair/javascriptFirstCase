9a767ea6b001b46d06afa428e7999a14ba6bddc6
EgemenNair <nairegemen@gmail.com>
2022-08-26 23:29:53 +0300

    Master3 | Case done!

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-// Defining the function
+// Defining the function:
 function oddishOrEvenish(num) {
-    return
-}
+    num = num.toString().split("").map(Number).reduce((a, b) => a + b, 0);  // Turn number to string, form an array, turn string array items to number type. Use reduce method to add all elements togather.
+    return (num % 2 == 1) ? "Odd" : "Even"; // If num%2 equals 1, return "Odd", else ; return "Even".
+};
